58e6fca9687aefa7b81678aebeebac841d3d2ecc
Rud09 <rudroy09@gmail.com>
2018-10-15 01:26:07 +0530

    fixed bugs

--- speech_gapi.py
+++ speech_gapi.py
@@ -2,5 +2,6 @@ import speech_recognition as sr
 r=sr.Recognizer()
 harvard=sr.AudioFile('harvard.wav')
 with harvard as source:
-    audio = r.record()
-r.recognize_google(audio)
+    audio = r.record(source)
+text=r.recognize_google(audio)
+print(text)
